91e29ecdd429ff13f11cfa06e20dc04323ff3530
Seokseong Jeon <6747391+somidad@users.noreply.github.com>
2024-06-10 21:37:51 +0900

    Update Next.js config for GitHub Pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,9 @@
+import { env } from 'process';
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    output: 'export',
+    distDir: env.NODE_ENV === 'production' ? 'docs' : '.next',
     experimental: {
         // Commented out temporarily
         // Probably need to be enabled
